89a39d02aab671ebb6e211520b794ea5c2dd3c60
Hamees Ahmed Khan <32523517+hameesakhan@users.noreply.github.com>
2024-10-31 14:37:23 -0500

    Update AzureStorageBlobServiceProvider.php

--- src/AzureStorageBlobServiceProvider.php
+++ src/AzureStorageBlobServiceProvider.php
@@ -23,6 +23,10 @@ final class AzureStorageBlobServiceProvider extends ServiceProvider
                 throw new \InvalidArgumentException('The [container] must be a string in the disk configuration.');
             }
 
+            if (isset($config['prefix']) && ! is_string($config['prefix'])) {
+                throw new \InvalidArgumentException('The [prefix] must be a string in the disk configuration.');
+            }
+            
             if (isset($config['root']) && ! is_string($config['root'])) {
                 throw new \InvalidArgumentException('The [root] must be a string in the disk configuration.');
             }
