4700ba65666a00db86f5a2a64303ea9340de27ee
brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
2026-01-16 16:50:15 +0100

    automatically push subtrees

---
+++ AzureStorageBlobAdapter.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AzureOss\Storage\BlobLaravel;
+
+use AzureOss\Storage\Blob\BlobServiceClient;
+use AzureOss\Storage\BlobFlysystem\AzureBlobStorageAdapter;
+use Illuminate\Filesystem\FilesystemAdapter;
+use League\Flysystem\Config;
+use League\Flysystem\Filesystem;
+
+/**
+ * @internal
+ *
+ * @property AzureBlobStorageAdapter $adapter
+ */
+final class AzureStorageBlobAdapter extends FilesystemAdapter
+{
+    /**
+     * Whether the configuration of this adapter allows temporary URLs.
+     */
+    public bool $canProvideTemporaryUrls;
+
+    /**
+     * @param  array{connection_string: string, container: string, prefix?: string, root?: string}  $config
+     */
+    public function __construct(array $config)
+    {
+        $serviceClient = BlobServiceClient::fromConnectionString($config['connection_string']);
+        $containerClient = $serviceClient->getContainerClient($config['container']);
+        $this->canProvideTemporaryUrls = $containerClient->canGenerateSasUri();
+        $adapter = new AzureBlobStorageAdapter($containerClient, $config['prefix'] ?? $config['root'] ?? '');
+
+        parent::__construct(
+            new Filesystem($adapter, $config),
+            $adapter,
+            $config,
+        );
+    }
+
+    public function url($path)
+    {
+        return $this->adapter->publicUrl($path, new Config);
+    }
+
+    /**
+     * Determine if temporary URLs can be generated.
+     *
+     * @return bool
+     */
+    public function providesTemporaryUrls()
+    {
+        return $this->canProvideTemporaryUrls;
+    }
+
+    /**
+     * Get a temporary URL for the file at the given path.
+     *
+     * @param  string  $path
+     * @param  \DateTimeInterface  $expiration
+     * @return string
+     */
+    /** @phpstan-ignore-next-line */
+    public function temporaryUrl($path, $expiration, array $options = [])
+    {
+        return $this->adapter->temporaryUrl(
+            $path,
+            $expiration,
+            new Config(array_merge(['permissions' => 'r'], $options)),
+        );
+    }
+
+    /**
+     * Get a temporary upload URL for the file at the given path.
+     *
+     * @param  string  $path
+     * @param  \DateTimeInterface  $expiration
+     * @return array{url: string, headers: array<string, string>}
+     */
+    /** @phpstan-ignore-next-line */
+    public function temporaryUploadUrl($path, $expiration, array $options = [])
+    {
+        $url = $this->adapter->temporaryUrl(
+            $path,
+            $expiration,
+            new Config(array_merge(['permissions' => 'cw'], $options)),
+        );
+        $contentType = isset($options['content-type']) && is_string($options['content-type'])
+            ? $options['content-type']
+            : 'application/octet-stream';
+
+        return [
+            'url' => $url,
+            'headers' => [
+                'x-ms-blob-type' => 'BlockBlob',
+                'Content-Type' => $contentType,
+            ],
+        ];
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+# Azure Storage Blob filesystem driver for Laravel
+
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/azure-oss/storage-blob-laravel.svg)](https://packagist.org/packages/azure-oss/storage-blob-laravel)
+[![Packagist Downloads](https://img.shields.io/packagist/dm/azure-oss/storage-blob-laravel)](https://packagist.org/packages/azure-oss/storage-blob-laravel)
+
+> [!IMPORTANT]
+> **Issues must be reported in the [monorepo issue tracker](https://github.com/Azure-OSS/azure-storage-monorepo/issues).** Please do not create issues in individual package repositories.
+
+## Minimum Requirements
+
+* PHP 8.1 or above
+
+## Install
+
+Install the package using composer:
+```shell
+composer require azure-oss/storage-blob-laravel
+```
+
+
+Then add this to the disks section of config/filesystems.php:
+```php
+'azure' => [ 
+    'driver' => 'azure-storage-blob',
+    'connection_string' => env('AZURE_STORAGE_CONNECTION_STRING'),
+    'container' => env('AZURE_STORAGE_CONTAINER'),
+],
+```
+
+## Usage
+
+Usage follows Laravel's filesystem conventions. For uploading, retrieving, and managing files, refer to the official Laravel documentation: 📖 [Laravel Filesystem Documentation](https://laravel.com/docs/12.x/filesystem)
+
+## Support
+
+Do you need help, do you want to talk to us, or is there anything else?
+
+Join us at:
+
+* [Github Discussions](https://github.com/Azure-OSS/azure-storage-monorepo/discussions)
+* [Slack](https://join.slack.com/t/azure-oss/shared_invite/zt-2lw5knpon-mqPM_LIuRZUoH02AY8uiYw)
+
+## License
+
+Azure-Storage-PHP-Adapter-Flysystem is released under the MIT License. See [LICENSE](./LICENSE) for details.
+
+## PHP Version Support Policy
+
+The maintainers of this package add support for a PHP version following its initial release and drop support for a PHP version once it has reached its end of security support.
